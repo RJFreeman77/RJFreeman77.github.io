5bae5c8febb8140356d977f8ea08df68c6023ede
RyanJF <ryanjfreeman77@gmail.com>
2018-10-09 17:53:47 -0500

    added javascript and firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDW52qZrfSBh9WNqBa-X7uDwCPtqU-w9sQ",
+    authDomain: "porfolio-4f174.firebaseapp.com",
+    databaseURL: "https://porfolio-4f174.firebaseio.com",
+    projectId: "porfolio-4f174",
+    storageBucket: "porfolio-4f174.appspot.com",
+    messagingSenderId: "189232253121"
+};
+firebase.initializeApp(config);
